a4e5005eea139a42389f78a02e7fb2ee3dd3c91b
Tayyaba Arshad <156302041+tayyaba-arshad@users.noreply.github.com>
2025-11-23 14:52:52 +0500

    change contact alert

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,5 +1,22 @@
 import { apiClient } from "../utils/apiClient.js";
 
+// Toast Function
+function showToast(message, type = "info") {
+  const toast = document.getElementById("toast");
+  
+  toast.textContent = message;
+
+  if (type === "success") toast.style.background = "#7ED957";
+  else if (type === "error") toast.style.background = "#dc3545";
+  else toast.style.background = "#333";
+
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 3000);
+}
+
 // Handle form submission
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".contact-form form");
@@ -20,11 +37,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       await apiClient.post("submitContact", { name, email, phone, comment });
-      alert("Your message has been sent successfully!");
+      showToast("Your message has been sent successfully!", "success");
       form.reset();
     } catch (err) {
       console.error("Unexpected error:", err);
-      alert(err.message || "Something went wrong!");
+      showToast(err.message || "Something went wrong!", "error");
     }
   });
 });
